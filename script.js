484e7502b76d07913ba1df6591515934bf6dc700
joakim ireri <joeahkim@gmail.com>
2024-09-13 16:06:15 +0300

    p

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 
 let currentSection = 0;
 const sections = document.querySelectorAll('.section');
